d7ed999c14d86954f91ee7e88272ebc3bf10cf7e
Flávio Meira <flavio.meira@luizalabs.com>
2025-02-18 13:00:41 -0300

    Solve Gestalt CSS import

--- src/components/TodoApp/TodoApp.js
+++ src/components/TodoApp/TodoApp.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react';
 import { Button, Text, TextField, Checkbox, Box, Fieldset, ColorSchemeProvider, SearchField, SelectList, Badge } from 'gestalt';
-import 'gestalt/dist/gestalt.css';
 
 const translations = {
   en: {
