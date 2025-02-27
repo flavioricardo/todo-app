ca6401190cd8c00f32be032ac80dade71d697a84
Flávio Meira <flavio.meira@luizalabs.com>
2025-02-27 02:48:58 -0300

    Remove unused logo and update manifest for To-Do App; add service worker for caching

--- src/index.js
+++ src/index.js
@@ -12,3 +12,16 @@ root.render(
 );
 
 reportWebVitals();
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker
+      .register('/serviceWorker.js')
+      .then((registration) => {
+        console.log('Service Worker registrado com sucesso!', registration);
+      })
+      .catch((error) => {
+        console.error('Erro ao registrar o Service Worker:', error);
+      });
+  });
+}
